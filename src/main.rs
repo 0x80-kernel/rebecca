d5698fc77f61f24b7febc3f5c703307952647a7a
mauwqq <mauw0@protonmail.com>
2025-03-11 16:56:57 -0300

    Bot connection login defined

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        match msg.content.as_str() {
+            "img" => {},
+            "mp3" => {},
+            "dll" => {},
+            &_ => {}
+        }
+        }
+}
+
+
+async fn login() -> Client {
+    let token: String = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let intents: GatewayIntents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::GUILD_MESSAGE_REACTIONS
+        | GatewayIntents::MESSAGE_CONTENT;
+    let client: Client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    return client;
+}
+    
+#[tokio::main]
+async fn main() {
+    let mut client: Client = login().await;
+    if let Err(e) = client.start().await {
+        println!("Client error: {e:?}");
+    }
 }
